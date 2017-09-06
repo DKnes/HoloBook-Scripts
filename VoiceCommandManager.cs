d5dbccacd0722b2dc64ee3c5ea244dc0aaf3e5e6
Mr.K <1061494593@qq.com>
2017-09-06 12:06:42 +0800

    Add voice command scripts Improve .gitignore

---
+++ VoiceCommandManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class VoiceCommandManager : MonoBehaviour,IFocusable {
+    public GameObject voiceHintPrefab;
+    private GameObject openVoiceHintPrefab;
+    public void OnFocusEnter()
+    {
+        
+    }
+
+    public void OnFocusExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        openVoiceHintPrefab = Instantiate(voiceHintPrefab);
+        openVoiceHintPrefab.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ VoiceHint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VoiceHint : MonoBehaviour {
+    [Tooltip("The message stating which voice command is available on this VoiceHint.")]
+    public Text userMessage;
+    public Color commandHeardColor = new Color(0.33f, 0.14f, 0.93f, 1.0f);
+
+    private Color startingColor;
+    
+
+    void Start()
+    {
+        startingColor = userMessage.color;
+    }
+
+    public void VoiceCommandHeard()
+    {
+        userMessage.color = commandHeardColor;
+    }
+
+    public void ResetTooltip()
+    {
+        userMessage.color = startingColor;
+    }
+}
