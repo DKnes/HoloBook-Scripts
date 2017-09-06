b3d53b6279c7ea08f2e83e99c14d529334f4e6ba
Mr.K <1061494593@qq.com>
2017-09-06 22:52:10 +0800

    Add  voice hint, easier to use voice command.

---
+++ UIButtonVoiceHintManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+public class UIButtonVoiceHintManager :Singleton<UIButtonVoiceHintManager>{
+    public GameObject allButtonVoiceHint;
+    public GameObject oneButtonVoiceHint;
+    public GameObject defaultDisableButton;
+
+    private GameObject[] UIButtons;
+	// Use this for initialization
+	void Start () {
+        defaultDisableButton.SetActive(true);
+        UIButtons = GameObject.FindGameObjectsWithTag("AllUIButton");
+        foreach (GameObject obj in UIButtons)
+        {
+            obj.AddComponent<UIVoiceCommandHelper>();
+        }
+        UIButtons = GameObject.FindGameObjectsWithTag("OneUIButton");
+        foreach (GameObject obj in UIButtons)
+        {
+            obj.AddComponent<UIVoiceCommandHelper>();
+        }
+        defaultDisableButton.SetActive(false);
+        
+	}
+	public void ShowVoiceHint(GameObject uiButton)
+    {
+        if(uiButton.tag=="AllUIButton")
+        {
+            allButtonVoiceHint.GetComponent<VoiceHint>().userMessage.text = "Say: \"" + uiButton.name + "\"";
+            allButtonVoiceHint.SetActive(true);
+        }
+        else if(uiButton.tag=="OneUIButton")
+        {
+            oneButtonVoiceHint.GetComponent<VoiceHint>().userMessage.text= "Say: \"" + uiButton.name + "\"";
+            oneButtonVoiceHint.SetActive(true);
+        }
+    }
+    public void QuitVoiceHint(GameObject uiButton)
+    {
+        if (uiButton.tag == "AllUIButton")
+        {
+            allButtonVoiceHint.SetActive(false);
+        }
+        else if (uiButton.tag == "OneUIButton")
+        {
+            oneButtonVoiceHint.SetActive(false);
+        }
+
+    }
+}
---
+++ UIVoiceCommandHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class UIVoiceCommandHelper : MonoBehaviour,IFocusable {
+    public void OnFocusEnter()
+    {
+        UIButtonVoiceHintManager.Instance.ShowVoiceHint(gameObject);
+    }
+
+    public void OnFocusExit()
+    {
+        UIButtonVoiceHintManager.Instance.QuitVoiceHint(gameObject);
+    }
+}
